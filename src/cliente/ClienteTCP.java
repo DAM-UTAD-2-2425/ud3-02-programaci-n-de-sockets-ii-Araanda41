13c9f8d704e32f08bce6bfc40b713dc8e684b067
Juan <teamzzesp@gmail.com>
2024-11-22 12:41:02 +0100

    Primer commit

--- src/cliente/ClienteTCP.java
+++ src/cliente/ClienteTCP.java
@@ -1,16 +1,37 @@
 package cliente;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+import javax.print.DocFlavor.CHAR_ARRAY;
+
 /**
- * TODO: Complementa esta clase para que genere la conexiÛn TCP con el servidor
+ * TODO: Complementa esta clase para que genere la conexi√≥n TCP con el servidor
  * para enviar un boleto, recibir la respuesta y finalizar la sesion
  */
 public class ClienteTCP {
+	private Socket socketCliente;
+	private BufferedReader entrada;
+	private PrintWriter salida;
 
 	/**
 	 * Constructor
 	 */
 	public ClienteTCP(String ip, int puerto) {
-
+		try {
+			socketCliente = new Socket(ip, puerto);
+			System.out.println("Conexi√≥n establecida: " + socketCliente);
+			entrada = new BufferedReader(new InputStreamReader(socketCliente.getInputStream()));
+			salida = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socketCliente.getOutputStream())), true);
+		} catch (IOException e) {
+			System.err.printf("Imposible conectar con ip: " + ip + "/ + puerto:" + puerto);
+			System.exit(-1);
+		}
 	}
 
 	/**
@@ -18,15 +39,38 @@ public class ClienteTCP {
 	 * @return respuesta del servidor con la respuesta del boleto
 	 */
 	public String comprobarBoleto(int[] combinacion) {
-		String respuesta = "Sin hacer";
+		
+		String linea = "";
+		//he pensado en guardarlo en un array de caracteres y enviarlo
+		for (int i = 0; i < combinacion.length; i++) {
+			linea.toCharArray()[i] =  (char) combinacion[i];
+		}
+		//envio la combinacion
+		salida.print(linea);
+		
+		String respuesta = null;
+		try {
+			respuesta = entrada.readLine();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return respuesta;
 	}
 
 	/**
-	 * Sirve para finalizar la la conexiÛn de Cliente y Servidor
+	 * Sirve para finalizar la la conexion de Cliente y Servidor
 	 */
-	public void finSesion () {
-		
+	public void finSesion() {
+		try {
+			salida.close();
+			entrada.close();
+			socketCliente.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println("-> Cliente Terminado");
+
 	}
-	
+
 }
--- src/servidor/ServidorTCP.java
+++ src/servidor/ServidorTCP.java
@@ -1,26 +1,42 @@
 package servidor;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.Random;
 import java.util.Set;
 import java.util.TreeSet;
 
 /**
- * TODO: Complementa esta clase para que acepte conexiones TCP con clientes
- * para recibir un boleto, generar la respuesta y finalizar la sesion
+ * TODO: Complementa esta clase para que acepte conexiones TCP con clientes para
+ * recibir un boleto, generar la respuesta y finalizar la sesion
  */
 public class ServidorTCP {
-	private String [] respuesta;
-	private int [] combinacion;
+	private String[] respuesta;
+	private int[] combinacion;
 	private int reintegro;
 	private int complementario;
+	// sockets del cliente y servidor
+	private Socket socketCliente;
+	private ServerSocket socketServidor;
+	// parametros de entrada y salida del socketCliente
+	private BufferedReader entrada;
+	private PrintWriter salida;
+
+	private int combinadaCliente[];
 
 	/**
 	 * Constructor
 	 */
-	public ServidorTCP (int puerto) {
-		this.respuesta = new String [9];
-		this.respuesta[0] = "Boleto inválido - Números repetidos";
-		this.respuesta[1] = "Boleto inválido - números incorretos (1-49)";
+	public ServidorTCP(int puerto) {
+		this.respuesta = new String[9];
+		this.respuesta[0] = "Boleto invalido - Numeros repetidos";
+		this.respuesta[1] = "Boleto invalido - numeros incorretos (1-49)";
 		this.respuesta[2] = "6 aciertos";
 		this.respuesta[3] = "5 aciertos + complementario";
 		this.respuesta[4] = "5 aciertos";
@@ -29,64 +45,101 @@ public class ServidorTCP {
 		this.respuesta[7] = "Reintegro";
 		this.respuesta[8] = "Sin premio";
 		generarCombinacion();
-		imprimirCombinacion();
+		try {
+			socketServidor = new ServerSocket(puerto);
+			System.out.println("Esperando a la conexiÃ³n con el cliente...");
+			socketCliente = socketServidor.accept();
+			System.out.println("ConexiÃ³n aceptada: " + socketCliente);
+			entrada = new BufferedReader(new InputStreamReader(socketCliente.getInputStream()));
+			salida = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socketCliente.getOutputStream())), true);
+			imprimirCombinacion();
+		} catch (IOException e) {
+			System.out.println("No puede escuchar en el puerto: " + puerto);
+			System.exit(-1);
+		}
 	}
-	
-	
+
 	/**
 	 * @return Debe leer la combinacion de numeros que le envia el cliente
 	 */
-	public String leerCombinacion () {
-		String respuesta = "Sin hacer leer";
-		return respuesta;
+	public String leerCombinacion() {
+		String linea = "";
+
+		try {
+			// recibo el string desde cliente, lo almaceno e un array de chars y lo itero en
+			// un bucle para alamcenar todo en el parametro de array de ints donde se guarda
+			// la combinada del cliente para luego comprobarla.
+			String recibidoCliente = entrada.readLine();
+			char[] charcombinadaCliente = recibidoCliente.toCharArray();
+			for (int i = 0; i < charcombinadaCliente.length; i++) {
+				combinadaCliente[i] = charcombinadaCliente[i];
+			}
+			// igualo linea a el String de antes para que se compruebe en el main si su
+			// valor es "fin"
+			linea = recibidoCliente;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return linea;
 	}
-	
+
 	/**
 	 * @return Debe devolver una de las posibles respuestas configuradas
 	 */
-	public String comprobarBoleto () {
-		String respuesta = "Sin hacer comprobar";
+	public String comprobarBoleto() {
+		String respuesta = "";
 		return respuesta;
 	}
 
 	/**
 	 * @param respuesta se debe enviar al ciente
 	 */
-	public void enviarRespuesta (String respuesta) {
-		
+	public void enviarRespuesta(String respuesta) {
+
 	}
-	
+
 	/**
 	 * Cierra el servidor
 	 */
-	public void finSesion () {
-		
+	public void finSesion() {
+		try {
+			salida.close();
+			entrada.close();
+			socketCliente.close();
+			socketServidor.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println("-> Servidor Terminado");
+
 	}
-	
+
 	/**
 	 * Metodo que genera una combinacion. NO MODIFICAR
 	 */
-	private void generarCombinacion () {
-		Set <Integer> numeros = new TreeSet <Integer>();
-		Random aleatorio = new Random ();
-		while (numeros.size()<6) {
-			numeros.add(aleatorio.nextInt(49)+1);
+	private void generarCombinacion() {
+		Set<Integer> numeros = new TreeSet<Integer>();
+		Random aleatorio = new Random();
+		while (numeros.size() < 6) {
+			numeros.add(aleatorio.nextInt(49) + 1);
 		}
 		int i = 0;
-		this.combinacion = new int [6];
+		this.combinacion = new int[6];
 		for (Integer elto : numeros) {
-			this.combinacion[i++]=elto;
+			this.combinacion[i++] = elto;
 		}
 		this.reintegro = aleatorio.nextInt(49) + 1;
 		this.complementario = aleatorio.nextInt(49) + 1;
 	}
-	
+
 	/**
 	 * Metodo que saca por consola del servidor la combinacion
 	 */
-	private void imprimirCombinacion () {
-		System.out.print("Combinación ganadora: ");
-		for (Integer elto : this.combinacion) 
+	private void imprimirCombinacion() {
+		System.out.print("CombinaciÃ³n ganadora: ");
+		for (Integer elto : this.combinacion)
 			System.out.print(elto + " ");
 		System.out.println("");
 		System.out.println("Complementario:       " + this.complementario);
@@ -94,4 +147,3 @@ public class ServidorTCP {
 	}
 
 }
-
